71340f4d703688d16124b16f9ccbcfcfd6a61539
joe <presbrey+git@gmail.com>
2013-09-29 02:40:56 -0400

    gofmt

--- main.go
+++ main.go
@@ -1,96 +1,108 @@
 package main
 
 import (
-    "flag"
-    "fmt"
-    "log"
-    "errors"
-    "bytes"
-    "github.com/mavricknz/ldap"
-    "crypto/tls"
-    "time"
-    "net/http"
-    "net/http/fcgi"
-    "encoding/base64"
-    "strings"
+	"bytes"
+	"crypto/tls"
+	"encoding/base64"
+	"errors"
+	"flag"
+	"fmt"
+	"github.com/mavricknz/ldap"
+	"log"
+	"net/http"
+	"net/http/fcgi"
+	"strings"
+	"time"
 )
 
 var (
-    userf  = flag.String("U", "uid=%s,cn=users,cn=accounts,dc=example,dc=com", "username template")
-    realm  = flag.String("r", "EXAMPLE.COM", "authentication realm")
-    host   = flag.String("h", "ldap.example.com", "LDAP server host")
-    port   = flag.Uint("p", 636, "LDAP server port")
-    ttl    = flag.Duration("t", 60*time.Second, "cache TTL")
+	userf = flag.String("U", "uid=%s,cn=users,cn=accounts,dc=example,dc=com", "username template")
+	realm = flag.String("r", "EXAMPLE.COM", "authentication realm")
+	host  = flag.String("h", "ldap.example.com", "LDAP server host")
+	port  = flag.Uint("p", 636, "LDAP server port")
+	ttl   = flag.Duration("t", 60*time.Second, "cache TTL")
 
-    ErrNoAuth = errors.New("http: no or invalid authorization header")
-    ErrHost = errors.New("http: no credential for provided host")
-    negotiate       = "Negotiate "
-    basic           = "Basic "
-    authorization   = "Authorization"
-    wwwAuthenticate = "Www-Authenticate"
+	ErrNoAuth       = errors.New("http: no or invalid authorization header")
+	ErrHost         = errors.New("http: no credential for provided host")
+	negotiate       = "Negotiate "
+	basic           = "Basic "
+	authorization   = "Authorization"
+	wwwAuthenticate = "Www-Authenticate"
 )
+
 func init() {
-    flag.Parse()
+	flag.Parse()
 }
 
-type Server struct {}
+type Server struct{}
 type entry struct {
-    valid bool
-    until time.Time
+	valid bool
+	until time.Time
 }
+
 var cache = make(map[string]*entry)
 var tlsConfig = &tls.Config{InsecureSkipVerify: true}
 var server = &Server{}
 
 func (s *Server) authenticate(username, password string) (r bool, e error) {
-    l := ldap.NewLDAPSSLConnection(*host, uint16(*port), tlsConfig)
-    e = l.Connect()
-    if e != nil { return }
+	l := ldap.NewLDAPSSLConnection(*host, uint16(*port), tlsConfig)
+	e = l.Connect()
+	if e != nil {
+		return
+	}
 
-    defer l.Close()
-    dn := fmt.Sprintf(*userf, username)
-    e = l.Bind(dn, password)
+	defer l.Close()
+	dn := fmt.Sprintf(*userf, username)
+	e = l.Bind(dn, password)
 
-    if e == nil { r = true }
-    return
+	if e == nil {
+		r = true
+	}
+	return
 }
 
 func splitAuth(h string) (string, []byte, error) {
-    i := strings.Index(h, " ")
-    if i < 0 {
-        return "", nil, ErrNoAuth
-    }
+	i := strings.Index(h, " ")
+	if i < 0 {
+		return "", nil, ErrNoAuth
+	}
 
-    data, err := base64.StdEncoding.DecodeString(h[i+1:])
-    return h[:i+1], data, err
+	data, err := base64.StdEncoding.DecodeString(h[i+1:])
+	return h[:i+1], data, err
 }
 
 func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-    w.Header().Add(wwwAuthenticate, fmt.Sprintf("Basic realm=\"%s\"", *realm))
+	w.Header().Add(wwwAuthenticate, fmt.Sprintf("Basic realm=\"%s\"", *realm))
 
-    _, data, err := splitAuth(r.Header.Get(authorization))
-    if err != nil { w.WriteHeader(401); return }
+	_, data, err := splitAuth(r.Header.Get(authorization))
+	if err != nil {
+		w.WriteHeader(401)
+		return
+	}
 
-    t := time.Now()
-    k := string(data)
-    e, ex := cache[k]
-    if ex && e.valid && t.Before(e.until) {
-        w.WriteHeader(200)
-        return
-    }
+	t := time.Now()
+	k := string(data)
+	e, ex := cache[k]
+	if ex && e.valid && t.Before(e.until) {
+		w.WriteHeader(200)
+		return
+	}
 
-    i := bytes.IndexRune(data, ':')
-    if i < 0 { w.WriteHeader(401); return }
-    username, password := string(data[:i]), string(data[i+1:])
-    valid, err := s.authenticate(username, password)
-    if valid {
-        cache[k] = &entry{valid:true,until:t.Add(*ttl)}
-        w.WriteHeader(200)
-    } else {
-        w.WriteHeader(401)
-    }
+	i := bytes.IndexRune(data, ':')
+	if i < 0 {
+		w.WriteHeader(401)
+		return
+	}
+	username, password := string(data[:i]), string(data[i+1:])
+	valid, err := s.authenticate(username, password)
+	if valid {
+		cache[k] = &entry{valid: true, until: t.Add(*ttl)}
+		w.WriteHeader(200)
+	} else {
+		w.WriteHeader(401)
+	}
 }
 
 func main() {
-    log.Fatal(fcgi.Serve(nil, server))
+	log.Fatal(fcgi.Serve(nil, server))
 }
